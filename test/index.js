00eee3a318fc3cbd07608c6efbaf7ba88137e54e
tobisek <jiri@wix.com>
2016-05-15 22:20:38 +0300

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,55 @@
+const expect = require('chai').expect;
+const babel = require('babel-core');
+const plugin = require('../lib/plugin');
+
+function transpile(source) {
+    return babel.transform(source, {
+        plugins: [plugin]
+    }).code;
+}
+
+
+function createTracker(source) {
+    const calls = [];
+    return {
+        __track__(start, end) {
+            calls.push(source.slice(start,end));
+        },
+
+        getCalls() {
+            return calls;
+        }
+    }
+
+}
+
+describe('Transpiler', function () {
+    it('creates self-tracking code', function () {
+        const source = `
+            const a = 1;
+            if(a === 0) {
+                console.log('This should be omitted...');
+            } else {
+                for(i=0;i<5;i++) {
+                    console.log(i);
+                }
+                console.log("That's about it.");
+            }
+        `;
+        const tracker = createTracker(source);
+        const __track__ = tracker.__track__;
+        const modifiedSource = transpile(source);
+        eval(modifiedSource);
+        expect(tracker.getCalls()).to.eql([
+            'const a = 1;',
+            'if(a === 0)',
+            'for(i=0;i<5;i++)',
+            'console.log(i);',
+            'console.log(i);',
+            'console.log(i);',
+            'console.log(i);',
+            'console.log(i);',
+            'console.log("That\'s about it.");'
+        ]);
+    });
+});
